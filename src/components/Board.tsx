5a756ce9b4ba834b453799d1740eb538ac830361
Warut Surapat <s.warut@gmail.com>
2019-02-17 22:15:18 +0900

    Laid project structure.

---
+++ src/components/Board.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+class Board extends React.Component {
+  public render() {
+    return (
+      <div className='board'>
+        This is a board.
+      </div>
+    )
+  }
+}
+
+export default Board;
